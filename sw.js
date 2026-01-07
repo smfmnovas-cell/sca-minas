07a4df5ea70405c5462c581dc1839e080c77840d
smfmnovas-cell <smfmnovas@gmail.com>
2026-01-07 10:15:47 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './web-app-manifest-192x192.png',
-  './web-app-manifest-512x192.png',
+  './icone-192.png',
+  './icone-512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
@@ -22,5 +22,4 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
   );
-
 });
